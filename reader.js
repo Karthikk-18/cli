3918a767255b9197d2388bbbfb9e063f0f816d3f
Karthik Yadav <karthikgudaru72@gmail.com>
2026-02-11 16:24:44 +0530

    added Asynchronous way

--- reader.js
+++ reader.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
 
 function readFileSync(filePath){
-   try {
-      const data = fs.readFileSync(filePath,'utf8');
-      console.log('--- file content ---');
-      console.log(data);
-      console.log('-- End of file ---')
-   } catch (error) {
-      console.error('Error reading File :',error.message);
-   }
+   fs.readFile(filePath,'utf-8',(error,data) => {
+       if(error){
+           console.log('Error reading in file : ',error.message);
+           return ;
+       }
+       console.log('--- File Content ---');
+        console.log(data);
+        console.log('--- End of File ---');
+   });
 }
 
-readFileSync('test.txt');
+async function readFileAsync(filePath) {
+   try {
+      const data = await fs.promises.readFile(filePath,'utf-8');
+      console.log('--- File Content ---');
+      console.log(data);
+      console.log('--- End of File ---');
+   } catch (error) {
+      console.error('Error reading file : ',error.message);
+   }
+
+}
+
+readFileAsync('test.txt');
